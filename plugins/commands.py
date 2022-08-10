86b65ae34041f13ab3aaf6a8dae75b5c8afdbc63
Fspb1gthub <96064031+Fspb1gthub@users.noreply.github.com>
2022-08-10 09:27:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,9 +31,7 @@ async def start(client, message):
             reply_markup=reply_markup,
         )
         return
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
-            await db.add_chat(message.chat.id, message.chat.title)
-        return 
+            
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
