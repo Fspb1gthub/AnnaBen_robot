6ae050a37390c7bdf3814251665558f0e970f1b0
Fspb1gthub <96064031+Fspb1gthub@users.noreply.github.com>
2022-08-10 08:47:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 @Client.on_message(filters.command("start"))
 async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
-        buttons = [
+        buttons = ([
             [
                 InlineKeyboardButton('💠𝙲𝙷𝙰𝙽𝙽𝙴𝙻💠', url='https://t.me/annaben_updates')
             ],
@@ -24,6 +24,7 @@ async def start(client, message):
                 InlineKeyboardButton('♻️𝙷𝙴𝙻𝙿♻️', url=f"https://t.me/{temp.U_NAME}?start=help"),
             ]
             ]
+                disable_web_page_preview=True)
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
