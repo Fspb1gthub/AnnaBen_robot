51a6e0df9ed1bcb6b65323f37f0e7e64064815e7
Fspb1gthub <96064031+Fspb1gthub@users.noreply.github.com>
2022-08-10 09:24:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,9 +31,6 @@ async def start(client, message):
             reply_markup=reply_markup,
         )
         return
-            await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
-        if not await db.get_chat(message.chat.id):
-            total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
