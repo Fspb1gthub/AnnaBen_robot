0f0d161afb7d9ae516acbdb06d7796a2f7791eca
Fspb1gthub <96064031+Fspb1gthub@users.noreply.github.com>
2022-08-18 22:48:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1044,11 +1044,11 @@ async def auto_filter(client, msg, spoll=False):
         cap = f"Here is what i found for your query {search}"
     if imdb and imdb.get('poster'):
         try:
-            await message.reply_text(caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_text(caption=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_photo(caption=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
         except Exception as e:
             logger.exception(e)
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
